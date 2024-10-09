8353e5a6f961d41a2f366f16e244275bdbfb5b8f
FromMilleto <jothas2000@hotmail.com>
2024-10-09 14:56:38 -0300

    Deleted the profile.jpg from the other activity and changed the css href to css/styles.css

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Verificação de Usuário</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
+
 <body>
     <div class="login-container">
         <h1>Login</h1>
@@ -29,4 +31,5 @@
         </form>
     </div>
 </body>
+
 </html>
